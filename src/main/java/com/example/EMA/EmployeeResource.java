9085c4714dfb1278b43e8a28ece803a566f282e5
kero0000 <chengmunchew@gmail.com>
2022-06-26 00:26:32 +0800

    cors

--- src/main/java/com/example/EMA/EmployeeResource.java
+++ src/main/java/com/example/EMA/EmployeeResource.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 @RestController
 @RequestMapping("/employee")
 public class EmployeeResource {
